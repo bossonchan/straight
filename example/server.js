391e614add799ab92613f058ee58538eeaac5309
陈柏信 <bossonchan@gmail.com>
2014-04-11 15:47:48 +0800

    update example

--- example/server.js
+++ example/server.js
@@ -4,47 +4,38 @@ var straight = require('../');
 var app = straight();
 
 // process before every event
-app.use(function (socket, request, response) {
+app.all(function (socket, request, response) {
   debug("handle event %s", request._event);
   response.next();
 });
 
 // use session middleware
-app.use(straight.session());
+app.all(straight.session());
 
-// create server and return io object
-var io = app.listen(3000);
-
-io.sockets.on('connection', function (socket) {
-
-  // wrap socket to hack the `on` function
-  var client = app.wrap(socket);
-
-  function requiresLogin (socket, request, response) {
-    debug('request: ', request);
-    var isLogin = request.session.name ? true : false;
-    if (isLogin) {
-      response.next();
-    } else {
-      response.ack({message: 'unauthorized'});
-    }
+function requiresLogin (socket, request, response) {
+  var isLogin = request.session.name ? true : false;
+  if (isLogin) {
+    response.next();
+  } else {
+    response.ack({message: 'unauthorized'});
   }
+}
 
-  function responseTest (socket, request, response) {
-    var session = request.session;
-    response.ack(session)
-    // or
-    socket.emit('response-test', session);
-  }
+function responseTest (socket, request, response) {
+  var session = request.session;
+  response.ack(session)
+  // or
+  socket.emit('response-test', session);
+}
 
-  function login (socket, request, response) {
-    request.session = {
-      name: 'bossonchan'
-    };
-    response.ack({result: 'success'});
-  }
+function login (socket, request, response) {
+  request.session = {
+    name: 'bossonchan'
+  };
+  response.ack({result: 'success'});
+}
 
-  client.on('request-test', requiresLogin, responseTest);
-  client.on('request-login', login);
-});
+app.on('request-test', requiresLogin, responseTest);
+app.on('request-login', login);
 
+app.listen(3000);
